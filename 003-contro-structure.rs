b16d33d5bdbe4b15220422a3a174b034aa09b132
phonnz <gonzalezm.alfonso@gmail.com>
2023-08-18 00:05:15 -0500

    Add basic if

---
+++ 003-contro-structure.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let a:  u32 = 11_000;
+    let b: u32 = 1;
+
+    if a < b {
+        println!("So A: {} is smaller than B: {}", a, b);
+    } else {
+        println!("Yeap!, A: {} is bigger than B: {}", a,b);
+    }
+
+    
+}
