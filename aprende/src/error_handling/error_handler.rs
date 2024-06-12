3b4b96484bf353b3dcee0a65ea0146bbd8205ca3
phonnz <gonzalezm.alfonso@gmail.com>
2024-06-13 01:51:15 +0200

    Add proper match for error handling

--- aprende/src/error_handling/error_handler.rs
+++ aprende/src/error_handling/error_handler.rs
@@ -16,9 +16,14 @@ pub fn file_loader() {
 }
 
 fn get_transactions(fname: &str) -> Result<Vec<Transaction>, String> {
-    //Err("No transactions".to_string())
     //    Ok(Vec::new())
-    let s = std::fs::read_to_string(fname).unwrap();
-    let t: Vec<Transaction> = serde_json::from_str(&s).unwrap();
+    let s = match std::fs::read_to_string(fname) {
+        Ok(r) => r,
+        Err(e) => return Err(e.to_string()),
+    };
+    let t: Vec<Transaction> = match serde_json::from_str(&s) {
+        Ok(v) => v,
+        Err(e) => return Err(e.to_string()),
+    };
     Ok(t)
 }
